7fb61b3e0497e382283e39917e13d7eee6845c50
Jonathon <jonathon.nordquist@gmail.com>
2016-04-28 09:51:23 -0700

    Add createLetterSubmit function in NewLetter.

--- app/assets/javascripts/components/_new_letter.js.jsx
+++ app/assets/javascripts/components/_new_letter.js.jsx
@@ -1,10 +1,16 @@
 var NewLetter = React.createClass({
+	createLetterSubmit() {
+		var applicant_name = this.refs.applicant_name.value;
+		var date = this.refs.date.value;
+
+		console.log(applicant_name + " -- " + date);
+	},
 	render() {
 		return(
 			<div>
 				<input ref='applicant_name' placeholder='Enter the appplicants name' />
 				<input ref='date' placeholder='Enter the interview date' />
-				<button>Create Letter</button>
+				<button onClick={this.createLetterSubmit}>Create Letter</button>
 			</div>
 		)
 	}
