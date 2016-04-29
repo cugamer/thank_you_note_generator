96dc4758dd2da5826fcb0850454b568707bdcccd
Jonathon <jonathon.nordquist@gmail.com>
2016-04-28 20:20:51 -0700

    Create LettersHelper module, refactor letter formatting into it

--- app/controllers/api/v1/letters_controller.rb
+++ app/controllers/api/v1/letters_controller.rb
@@ -5,16 +5,10 @@ class Api::V1::LettersController < Api::V1::BaseController
 
 	def create
 		newLetter = Letter.create(letter_params)
-		letterGreeting = "Dear #{newLetter.interviewer_name}"
-		letterBody = "It was a pleasure to speak to you on #{newLetter.date} regarding the position of #{newLetter.job}.  As I mentioned in the interview I am very #{newLetter.quality} and I'm certain you'll agree that this makes me perfect for this position.  I look forward to hearing back from you soon regarding your decision."
-		letterSalutation = newLetter.salutation
-		letterName = newLetter.applicant_name
-		ActiveSupport.escape_html_entities_in_json = true 
-		render :json => {id: newLetter.id, 
-						letterGreeting: letterGreeting,
-						letterBody: letterBody,
-						letterSalutation: letterSalutation,
-						letterName: letterName}
+		
+		formatLetter(newLetter)
+
+		render :json => formatLetter(newLetter)
 	end
 
 	private
---
+++ app/helpers/letters_helper.rb
@@ -0,0 +1,15 @@
+module LettersHelper
+	def formatLetter(newLetter)
+		letterGreeting = "Dear #{newLetter.interviewer_name}"
+		letterBody = "It was a pleasure to speak to you on #{newLetter.date} regarding the position of #{newLetter.job}.  As I mentioned in the interview I am very #{newLetter.quality} and I'm certain you'll agree that this makes me perfect for this position.  I look forward to hearing back from you soon regarding your decision."
+		letterSalutation = newLetter.salutation
+		letterName = newLetter.applicant_name
+
+		letterOutput = {id: newLetter.id, 
+						letterGreeting: letterGreeting,
+						letterBody: letterBody,
+						letterSalutation: letterSalutation,
+						letterName: letterName}
+		return letterOutput
+	end
+end
