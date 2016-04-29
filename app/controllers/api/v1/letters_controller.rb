b31a64b3f84dbf0704d246019de5c3c89b03d767
Jonathon <jonathon.nordquist@gmail.com>
2016-04-28 19:35:08 -0700

    Create template to insert data into formatted letter.

--- app/controllers/api/v1/letters_controller.rb
+++ app/controllers/api/v1/letters_controller.rb
@@ -4,7 +4,11 @@ class Api::V1::LettersController < Api::V1::BaseController
 	end
 
 	def create
-		respond_with :api, :v1, Letter.create(letter_params)
+		letter = Letter.create(letter_params)
+		outputLetter = "Dear #{letter.interviewer_name},\n\nIt was a pleasure to speak to you on #{letter.date} regarding the position of #{letter.job}.  As I mentioned in the interview I am very #{letter.quality} and I'm certian you'll agree that this makes me perfect for this position.  I look forward to hearing back from you soon regarding your decision.\n\n#{letter.salutation}\n\n#{letter.applicant_name}"
+		p "-----------------------------------"
+		puts outputLetter
+		respond_with :api, :v1, letter
 	end
 
 	def show
