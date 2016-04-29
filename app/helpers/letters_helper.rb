4988cac09a656d44e7920c1600b4f559c9c90637
Jonathon <jonathon.nordquist@gmail.com>
2016-04-28 22:06:51 -0700

    Add commas to letter output

--- app/helpers/letters_helper.rb
+++ app/helpers/letters_helper.rb
@@ -1,8 +1,8 @@
 module LettersHelper
 	def formatLetter(newLetter)
-		letterGreeting = "Dear #{newLetter.interviewer_name}"
+		letterGreeting = "Dear #{newLetter.interviewer_name},"
 		letterBody = "It was a pleasure to speak to you on #{newLetter.date} regarding the position of #{newLetter.job}.  As I mentioned in the interview I am very #{newLetter.quality} and I'm certain you'll agree that this makes me perfect for this position.  I look forward to hearing back from you soon regarding your decision."
-		letterSalutation = newLetter.salutation
+		letterSalutation = "#{newLetter.salutation},"
 		letterName = newLetter.applicant_name
 
 		letterOutput = {id: newLetter.id, 
